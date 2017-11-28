b290bda197b0bd452154cb1b618e3f9649bb0e10
Christoph Uran <c.uran@fh-kaernten.at>
2017-11-28 16:51:21 +0100

    Did not commit everything in the last commit.

--- GeneticConflictGeneration/src/main/java/org/eclipse/agail/genconfgen/QuickXPlain.java
+++ GeneticConflictGeneration/src/main/java/org/eclipse/agail/genconfgen/QuickXPlain.java
@@ -1,15 +1,14 @@
-/*******************************************************************************
- * Copyright (C) 2017 TUGraz.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     TUGraz - initial implementation
- ******************************************************************************/
+/*********************************************************************
+* Copyright (c) 2017-11-28 Christoph Uran (TU Graz)
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
 
-package at.tugraz.ist.ase.genconfgen;
+package org.eclipse.agail.genconfgen;
 
 import java.util.ArrayList;
 
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Ramp-up Configurator
-Maintainer: Seda Polat Erdeniz, TUGraz
 
-Monitoring Station Configurator for Pilot-C.
+Maintainer: Christoph Uran, TUGraz
 
-Ramp-up Configurator is hosted as a web application to help AGILE users for setting up their initial environment.
+Includes a Smarthome Configurator (smarthome) for testing purposes, an Air Pollution Monitoring Station Configurator (polmon) for Pilot-C and a tool for the automated a priori generation of conflicting user requirements using a genetic algorithm (GeneticConflictGeneration).
+
+The Ramp-up Configurator is hosted as a web application to help AGAIL users for setting up their initial environment.
